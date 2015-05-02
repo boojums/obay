10ea57bc6afe31729bb907e90af97aaaf06fb161
Cristina <cristina.luis@gmail.com>
2015-05-02 02:07:16 +0200

    added filter for bid

--- obay/admin.py
+++ obay/admin.py
@@ -4,14 +4,13 @@ from obay.models import Auction, Item, Bid, UserProfile
 class AuctionAdmin(admin.ModelAdmin):
     list_display = ('name', 'start', 'end', 'description', 'is_active')
 
-# TODO: display other fields
 class ItemAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
     list_display = ('name', 'auction', 'donor', 'approved', 'category')
 
 class BidAdmin(admin.ModelAdmin):
     list_display = ('item', 'time', 'amount', 'user')
-
+    list_filter = ('item',)
 
 # Register your models here.
 admin.site.register(Auction, AuctionAdmin)
