a4d05684c108ea3abca0c8fd0fdcc1d352bd1a7f
Cristina <cristina.luis@gmail.com>
2015-05-10 15:13:46 +0200

    cirspy bid form

--- obay/forms.py
+++ obay/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from django.core.urlresolvers import reverse
 from django.core.exceptions import ValidationError
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Fieldset, ButtonHolder
-from crispy_forms.bootstrap import FormActions, PrependedAppendedText
+from crispy_forms.bootstrap import FormActions, PrependedAppendedText, StrictButton
 
 from obay.models import Item, Bid, User, UserProfile
 
@@ -31,15 +32,6 @@ class ItemForm(forms.ModelForm):
                 )
             )
         self.fields['description'].widget.attrs['rows'] = 4
-
-    # name = forms.CharField(max_length=50)
-    # description = forms.CharField()
-    # donor = forms.CharField(max_length=128)
-    # pic = forms.ImageField()
-    # category = forms.ChoiceField(choices=Item.CATEGORY_CHOICES)
-    # slug = forms.CharField(widget=forms.HiddenInput(), required=False)
-    # auction = forms.CharField(widget=forms.HiddenInput(), required=False)
-    # contact = forms.CharField(widget=forms.HiddenInput(), required=False)
     
     class Meta:
         model = Item
@@ -51,6 +43,16 @@ class BidForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         self.item = kwargs.pop('item', None)
         super(BidForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_method = 'POST'
+        self.helper.form_action = reverse('item', kwargs={'item_name_slug': self.item.slug}) 
+        self.helper.form_class = 'form-inline'
+        self.helper.form_show_labels = False
+        self.helper.layout = Layout(
+            PrependedAppendedText('amount', '$', '.00'),
+            Submit('submit', 'Bid', css_class='btn-primary')
+            )
+
 
     class Meta:
         model = Bid
@@ -75,3 +77,4 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('club', 'isteam')
+
